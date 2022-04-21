a040b2ad5972ef99ae5c7033b2fed9b76969c581
jazzboy65 <alexis.gordeeff@gmail.com>
2022-04-21 23:02:20 +0500

    task 5,6

--- src/main/java/com/alexgordeev/spring/mvc/MyController.java
+++ src/main/java/com/alexgordeev/spring/mvc/MyController.java
@@ -1,7 +1,11 @@
 package com.alexgordeev.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -16,8 +20,30 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails() {
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+////    public String showEmpDetails(HttpServletRequest request, Model model) {
+////
+////        String empName = request.getParameter("employeeName");
+////        empName = "Mr. " + empName;
+////        model.addAttribute("nameAttribute",empName);
+////
+////        model.addAttribute("description", "- java senior fullstack developer");
+////
+////        return "show-emp-details-view";
+////    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails() {
+    public String showEmpDetails(@RequestParam("employeeName") String empName
+            , Model model) {
+
+        empName = "Mr. " + empName + "!";
+        model.addAttribute("nameAttribute",empName);
+
         return "show-emp-details-view";
     }
 }
