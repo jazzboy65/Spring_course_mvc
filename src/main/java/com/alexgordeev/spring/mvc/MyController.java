fa030c276d2efe5dd84cf30b83c9e8b05f13fa6a
jazzboy65 <alexis.gordeeff@gmail.com>
2023-11-30 22:43:41 +0500

    small up

--- src/main/java/com/alexgordeev/spring/mvc/MyController.java
+++ src/main/java/com/alexgordeev/spring/mvc/MyController.java
@@ -3,9 +3,7 @@ package com.alexgordeev.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -49,7 +47,7 @@ public class MyController {
 //        return "show-emp-details-view";
 //    }
 
-    @RequestMapping("/showDetails")
+    @PostMapping( "/showDetails")
 //    public String showEmpDetails(@RequestParam("employeeName") String empName
 //                , Model model) {
     public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp
